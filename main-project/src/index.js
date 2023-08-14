d1a991af8a5b690a67ea67ca955eb6f435769207
Abhay-dobariya <abhaydobariya96@gmail.com>
2023-08-14 21:46:50 +0530

    update

--- main-project/src/index.js
+++ main-project/src/index.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 const server = http.createServer(app)
 
 server.listen(config.port, () => {
-     console.log("server listning port number 3000!");
+     console.log("server listning port number 8585!");
    });
 
 
