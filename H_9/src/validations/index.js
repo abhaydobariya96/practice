fc8a983dc073c197cab03399ffd95dd2035d47bd
Abhay-dobariya <abhaydobariya96@gmail.com>
2023-08-26 00:41:48 +0530

    h_9 all done

--- H_9/src/validations/index.js
+++ H_9/src/validations/index.js
@@ -6,5 +6,10 @@ module.exports.busValidation = require("./bus.vlaidation");
 module.exports.schoolValidation = require("./school.validation");
 module.exports.travelValidation = require("./travel.validation");
 module.exports.jewelleryValidation = require("./jewellery.validation");
+module.exports.stationaryValidation = require("./stationary.validation");
+module.exports.pharmacyValidation = require("./pharmacy.validation");
+module.exports.movieValidation = require("./movie.validation");
+module.exports.musicValidation = require("./music.validation");
+module.exports.groceryValidation = require("./grocery.validation");
 
 // module.exports.tokenValidation = require("./token.validation");
